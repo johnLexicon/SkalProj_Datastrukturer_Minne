fbabc2e81d51ffdf52728e5852ca4e7fb517730e
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-11 08:00:53 +0100

    Tests for CheckParentheses

--- DataStrukturer_Minne_Tests/Test.cs
+++ DataStrukturer_Minne_Tests/Test.cs
@@ -7,6 +7,7 @@ namespace DataStrukturer_Minne_Tests
     [TestFixture()]
     public class Test
     {
+
         [TestCase(1, 2)]
         [TestCase(2, 4)]
         [TestCase(3, 6)]
@@ -17,6 +18,36 @@ namespace DataStrukturer_Minne_Tests
 
             //Act
             int actual = RecursiveEven(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("[({})]")]
+        [TestCase("List<int> lista = new List<int>() { 2, 3, 4 };")]
+        [TestCase("no parentheses")]
+        public void CheckParentheses_CorrectValues_Test(string wellFormed)
+        {
+            //Arrange
+            bool expected = true;
+
+            //Act
+            bool actual = CheckParantheses(wellFormed);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("({)}")]
+        [TestCase("[")]
+        [TestCase("{})")]
+        public void CheckParentheses_WrongValues_Test(string malFormed)
+        {
+            //Arrange
+            bool expected = false;
+
+            //Act
+            bool actual = CheckParantheses(malFormed);
 
             //Assert
             Assert.AreEqual(expected, actual);
